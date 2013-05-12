8ee271ebe712b763cc99b9a7a29012dbfb4c10e5
Kris Vigfusson <kvigfusson@gmail.com>
2013-05-12 13:08:08 -0500

    Modified user test

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -11,7 +11,7 @@ class StatusesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:statuses)
   end
 
-  test "should be redirected when  not logged in" do
+  test "should be redirected when not logged in" do
     get :new
     assert_response :redirect
     assert_redirected_to :new_user_session
@@ -23,7 +23,14 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create status" do
+  test "should be logged in to post a status" do
+    post :create, status: { content: "Hello"}
+    assert_response :redirect
+    assert_redirected_to :new_user_session
+  end
+
+  test "should create status when logged in" do
+    sign_in users(:kris)
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
@@ -36,17 +43,20 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should get edit when logged in" do
+    sign_in users(:kris)
     get :edit, id: @status
     assert_response :success
   end
 
-  test "should update status" do
+  test "should redirect status update when not logged in" do
     put :update, id: @status, status: { content: @status.content }
-    assert_redirected_to status_path(assigns(:status))
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
   end
 
-  test "should destroy status" do
+  test "should destroy status when logged in" do
+    sign_in users(:kris)
     assert_difference('Status.count', -1) do
       delete :destroy, id: @status
   end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -28,11 +28,21 @@ test "a user should enter a profile name" do
   end
 
   test "a user should have a profile name without spaces" do
-  	user = User.new
+  	user = User.new(first_name: 'Kris', last_name: 'Vigfusson', email: 'kvigfusson2@gmail.com')
+    user.password = user.password_confirmation = 'krisv123'
+    
   	user.profile_name = "My Profile With Spaces"
 
   	assert !user.save
   	assert !user.errors[:profile_name].empty?
   	assert user.errors[:profile_name].include? ("Must be formatted correctly.")
+  end
+
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'Kris', last_name: 'Vigfusson', email: 'kvigfusson2@gmail.com')
+    user.password = user.password_confirmation = 'krisv123'
+
+    user.profile_name = 'krizpy869'
+    assert user.valid?
   end
 end
