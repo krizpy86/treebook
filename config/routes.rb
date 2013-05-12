1b588193fe35ba68958e2ee6f5a5ed691267fbc1
Kris Vigfusson <kvigfusson@gmail.com>
2013-05-12 14:30:26 -0500

    Updated profile page & added scoped status updates to it

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Treebook::Application.routes.draw do
   get 'feed',to: "statuses#index"
   root to: 'statuses#index'
 
-
+  get '/:id', to: 'profiles#show'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
