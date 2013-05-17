559bdc0c3001815dc94c8f2a3d7d508117a28057
Kris Vigfusson <kvigfusson@gmail.com>
2013-05-17 12:42:04 -0500

    Added gravatar, etc

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Treebook::Application.routes.draw do
   root to: 'statuses#index'
 
   get '/:id', to: 'profiles#show'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
